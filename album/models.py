cb00116f9fb7d7d61ada0aaf388d32d4e0f416ae
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-05-11 21:46:00 +0300

    Add CRUD functions and tests for Image model class.

--- album/models.py
+++ album/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length =30)
+    description = models.TextField()
+    image = models.ImageField(upload_to = 'images/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image_by_id(cls,number):
+        return cls.objects.get(pk = number)
+        
