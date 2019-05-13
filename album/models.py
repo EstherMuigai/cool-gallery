8cc124940a77d8734a6f4c22c09da5c3af99e84e
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-05-13 22:40:18 +0300

    Add search functionality by category.

--- album/models.py
+++ album/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def save_location(self):
+        self.save()
+    def delete_location(self):
+        self.delete()
+
+    @classmethod
+    def filter_by_location(cls,search_term):
+        images = cls.objects.filter(name__icontains=search_term)
+        return images
+
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     name = models.CharField(max_length =30)
 
@@ -23,6 +39,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     upload_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category,blank =True)
+    location = models.ForeignKey(Location,blank =True,null=True)
 
     def save_image(self):
         self.save()
@@ -37,19 +54,3 @@ class Image(models.Model):
     def __str__(self):
         return self.name
 
-class Location(models.Model):
-    name = models.CharField(max_length =30)
-    image = models.ForeignKey(Image)
-
-    def save_location(self):
-        self.save()
-    def delete_location(self):
-        self.delete()
-
-    @classmethod
-    def filter_by_location(cls,search_term):
-        images = cls.objects.filter(name__icontains=search_term)
-        return images
-
-    def __str__(self):
-        return self.name
