9b024a5ee2fc4b9f2feab43403fc764e1cda3d66
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-05-13 22:49:13 +0300

    Add a location list.

--- album/views.py
+++ album/views.py
@@ -5,12 +5,14 @@ from .models import Image,Category,Location
 # Create your views here.
 def welcome(request):
     categories = Category.objects.all()
-    return render(request, 'landingpage.html',{"categories":categories})
+    locations = Location.objects.all()
+    return render(request, 'landingpage.html',{"categories":categories,"locations":locations})
 
 def categories(request):
     categories = Category.objects.all()
+    locations = Location.objects.all()
     images = Image.objects.all()
-    return render(request, 'categories.html',{"categories":categories,"images":images})
+    return render(request, 'categories.html',{"categories":categories,"images":images,"locations":locations})
 
 def category(request):
     images = Image.objects.all()
@@ -18,13 +20,13 @@ def category(request):
         search_term = request.GET.get("category")
         searched_category = Category.search_by_category(search_term)
         message = f"{search_term}"
-        return render(request, 'category.html',{"message":message,"category": searched_category,"images":images})
+        return render(request, 'category.html',{"message":message,"category": searched_category,"images":images,"locations":locations})
 
     else:
         message = "Invalid."
-        return render(request, 'landingpage.html',{"message":message})
+        return render(request, 'landingpage.html',{"message":message,"locations":locations})
 
 def location(request):
-    return render(request, 'location.html')
+    return render(request, 'location.html', {"locations":locations})
 
 
